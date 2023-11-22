2c283713a6a007e77c345944c3dcb21661303591
orroo <omarriahi2003@gmail.com>
2023-11-22 19:20:05 +0100

    ajout_de_controle

--- view/update.php
+++ view/update.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -16,42 +17,42 @@
             <button class="payment">log in</button>
         </nav>
     </header>
-
-    <form class="board" action="http://localhost/P/view/updateplan.php?id=<?php echo $_POST["id"]; ?>" method="post">
+    <form class="board" action="http://localhost/P/view/updateplan.php?id=<?php echo $_POST["id"]; ?>" method="post"  onsubmit="return VERIFplan()">
         <div>  
             <label class="container">
-                <input type="checkbox" name="med" value="y">
+                <input type="checkbox" name="med" id="med" value="y">
                 <span class="checkmark"></span>
             </label>
             <a>mediation</a>
         </div>
         <div>  
             <label class="container">
-                <input type="checkbox" name="test"value="y">
+                <input type="checkbox" name="test" id="test" value="y">
                 <span class="checkmark"></span>
             </label>
             <a>test</a>
         </div>
         <div>  
             <label class="container">
-                <input type="checkbox" name="psy" value="y">
+                <input type="checkbox" name="psy" id="psy" value="y">
                 <span class="checkmark"></span>
             </label>
             <a>psychiatry</a>
         </div>
         <div>  
             <label class="container">
-                <input type="checkbox" name="ther" value="y">
+                <input type="checkbox" name="ther" id="ther" value="y">
                 <span class="checkmark"></span>
             </label>
             <a>therapy</a>
         </div>
         <div>
-            <input type="submit" value="update">
+            <input type="submit" value="update" id="SUB">
             <input type="reset" value="clear">
         </div>
 
     </form>
 
+    <script src="../controller/select.js"></script>
 </body>     
 </html>
